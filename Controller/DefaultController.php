70c9efc3f37cdbb4c53ee700e73c268117d035f7
Matthew Ratzke <matthew.003@me.com>
2011-12-04 21:43:45 -0700

    MongoDB Basic Config

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace AirOS\DashboardBundle\Controller;
 
-use AirOS\DashboardBundle\Event\ControlCenterEvents;
+use AirOS\DashboardBundle\Event\DashboardEvents;
 use AirOS\DashboardBundle\Event\InitAdminDesktopEvent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,9 +16,12 @@ class DefaultController extends Controller
 
     public function indexAction()
     {
+    	//Get required variables
+    	//TODO: Get user authentication level/type
     	$this->session = $this->getRequest()->getSession();
-    	$this->event = new InitAdminDesktopEvent($this->session);
     	$this->dispatcher = $this->container->get('event_dispatcher');
+		
+    	$this->event = new InitAdminDesktopEvent($this->session);
     	$this->dispatcher->dispatch(ControlCenterEvents::InitAdminDesktop, $this->event);
         return new Response('<html><body>Hello World!'. $this->session->get('test2') .'</body></html>');
     }
--- Event/DashboardEvents.php
+++ Event/DashboardEvents.php
@@ -3,6 +3,9 @@ namespace AirOS\DashboardBundle\Event;
 
 final class DashboardEvents
 {
+	/**
+	 * Init Dashboard Events
+	 */
     /**
      * The admin_dashboard.init event is thrown each time the system attempts to create
      * the admin desktop.
@@ -24,5 +27,55 @@ final class DashboardEvents
      * @var string
      */
     const InitUserDashboard = 'user_dashboard.init';
+
+	/**
+	 * Load Dashboard Events
+	 */
+    /**
+     * The admin_dashboard.init event is thrown each time the system attempts to create
+     * the admin desktop.
+     *
+     * The event listener receives an AirOS\DashboardBundle\Event\InitAdminDashboardEvent
+     * instance.
+     *
+     * @var string
+     */
+    const LoadAdminDashboard = 'admin_dashboard.load';
+
+    /**
+     * The user_dashboard.init event is thrown each time the system attempts to create
+     * the user desktop.
+     *
+     * The event listener receives an AirOS\DashboardBundle\Event\InitUserDashboardEvent
+     * instance.
+     *
+     * @var string
+     */
+    const LoadUserDashboard = 'user_dashboard.load';
+
+	/**
+	 * Deploy Dashboard Events
+	 */
+    /**
+     * The admin_dashboard.init event is thrown each time the system attempts to create
+     * the admin desktop.
+     *
+     * The event listener receives an AirOS\DashboardBundle\Event\InitAdminDashboardEvent
+     * instance.
+     *
+     * @var string
+     */
+    const DeployAdminDashboard = 'admin_dashboard.deploy';
+
+    /**
+     * The user_dashboard.init event is thrown each time the system attempts to create
+     * the user desktop.
+     *
+     * The event listener receives an AirOS\DashboardBundle\Event\InitUserDashboardEvent
+     * instance.
+     *
+     * @var string
+     */
+    const DeployUserDashboard = 'user_dashboard.deploy';
 }
 ?>
